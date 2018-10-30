32eb9a9812bcb6c73e3bbd6173e2c5d04acadc08
Niharika <nsundarigari@gmail.com>
2018-10-30 00:18:29 -0400

    !!

--- js/verb_02.js
+++ js/verb_02.js
@@ -31,23 +31,47 @@ function randomFloat (min, max) {
   return parseFloat(value.toFixed(2));
 }
 
+function hslColor(h, s, l) {  
+  var hue = h;
+  var saturation = s + "%";
+  var luminance = l + "%";
+
+  var color = "hsl(" + hue + "," + saturation + "," + luminance + ")";
+
+  return color;
+}
+
+
 var shape1 = document.querySelector(".shape1");
 
-//var width= randomNumber(150,700);
-//var height= randomNumber(150, 1000);
 
-//shape1.style.width = width + "px";
-//shape1.style.height = height + "px";
+var shape2 = document.querySelector(".shape2");
 
+var R1= randomNumber(150,500);
+var R2= randomNumber(150,600);
 
-var h = randomNumber(0, 359);
+shape1.style.width = R1 + "px";
+shape1.style.height = R1 + "px";
 
-shape1.style.background = hslColor(h, 50, 50);
-shape1.style.borderColor = hslColor(h, 90, 50);
+shape2.style.width = R2 + "px";
+shape2.style.height = R2 + "px";
+
+var h1 = randomNumber(0, 359);
+
+shape1.style.background = hslColor(h1, 50, 50);
+shape1.style.borderColor = hslColor(h1, 90, 50);
+
+var h2 = randomNumber(0, 359);
+
+shape2.style.background = hslColor(h2, 50, 50);
+shape2.style.borderColor = hslColor(h2, 90, 50);
 
 var container = document.querySelector(".content");
 
-container.style.background = hslColor(h, 50, 70);
+
+var h3 = randomNumber(0, 359);
+
+container.style.background = hslColor(h3, 50, 70);
 
 
 
